7dcbdffea5618a9bdff896c2f4686a6fbd251344
rabiagizemdemirhan <rabiagizemdemirhan@gmail.com>
2026-05-14 06:44:49 +0300

    Connect PostgreSQL database

--- database.py
+++ database.py
@@ -1,24 +1,37 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./kuliss.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Render postgres URL fix
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
 
 engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
-    autocommit=False, 
-    autoflush=False, 
+    autocommit=False,
+    autoflush=False,
     bind=engine
 )
 
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
+
     finally:
         db.close()
